a361e349ec8210d3b8eab66468fd0566d18edd3d
sanchuah <sanchuah@andrew.cmu.edu>
2015-12-10 14:30:41 -0500

    update color

--- init.js
+++ init.js
@@ -12,14 +12,20 @@ function init() {
 // Basic options for a simple Google Map
 // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 var mapOptions = {
+zoomControl: false,
+mapTypeControl: false,
+scaleControl: false,
+streetViewControl: false,
+rotateControl: false,
+
 // How zoomed in you want the map to start at (always required)
-zoom: 3,
+zoom: 4,
 
 // The latitude and longitude to center the map (always required)
 center: new google.maps.LatLng(avg_lat, avg_lng),
 
 // How you would like to style the map.
-styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}, {featureType: "administrative", elementType: "geometry", stylers: [{visibility:"off"}]}, {featureType: "administrative.country", elementType: "labels", stylers: [{visibility:"off"}]}, {featureType: "administrative.country", elementType: "geometry", stylers: [{visibility:"on"}]}]
+styles: [ {"featureType":"administrative", "elementType":"all", "stylers":[{"visibility":"off"}]}, {"featureType":"landscape", "elementType":"labels", "stylers":[{"visibility":"off"}]}, {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#eeeeee"},{"visibility":"on"}, {"lightness":-20}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ffffff"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]},{featureType: "administrative.country", elementType: "labels", stylers: [{visibility:"off"}]}, {featureType: "administrative.country", elementType: "geometry", stylers: [{visibility:"on"}]}]
 };
 
 // Get the HTML DOM element that will contain your map
